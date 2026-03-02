58007c0becf5ae1e2fce92c68753e5f6fad7bacb
Gray_Advantage <sergeyindra@yandex.ru>
2026-03-02 13:07:35 +0300

    fix: cors...?

--- api/main.py
+++ api/main.py
@@ -36,7 +36,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:8001"],
+    allow_origin_regex=r"^https?://(localhost|127\.0\.0\.1)(:\d+)?$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
